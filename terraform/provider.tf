0db653c98743ccc91d1ef69053d2d4a5e3ef0afa
Devanshi Kevat <135352265+DevanshiKevat@users.noreply.github.com>
2026-05-04 17:35:17 +0530

    upd add organization name

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -8,9 +8,10 @@ terraform {
 }
 
 provider "snowflake" {
-  account  = var.snowflake_account
-  username = var.snowflake_user
+  organization_name = var.snowflake_organization
+  account_name      = var.snowflake_account
+
+  user     = var.snowflake_user
   password = var.snowflake_password
   role     = var.snowflake_role
-  region   = var.snowflake_region
 }
