31504283ca8992ee885f53650aa3bfaa5fe91395
Devanshi Kevat <135352265+DevanshiKevat@users.noreply.github.com>
2026-05-04 17:27:09 +0530

    upd providers file

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflake-Labs/snowflake"
+      version = "~> 0.80"
+    }
+  }
+}
+
 provider "snowflake" {
   account  = var.snowflake_account
   username = var.snowflake_user
